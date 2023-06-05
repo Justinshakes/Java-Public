b26feffb65468557c5f34f10fb51cbcc51f864c9
Justinshakes <Superjustin44@gmail.com>
2023-06-04 19:47:22 -0700

    Update

---
+++ code-wars/src/RemoveChars.java
@@ -0,0 +1,13 @@
+public class RemoveChars {
+    public static String remove(String str) {
+        if (str.length() < 2) {
+            return str; // Return the original string if it has less than two characters
+        }
+        return str.substring(1, str.length() - 1);
+    }
+
+    public static void main(String[] args) {
+        String modifiedString = "Elephant";
+        System.out.println(remove(modifiedString));
+    }
+}
