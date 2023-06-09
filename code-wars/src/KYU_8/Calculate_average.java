88bd2927b8fd793ecbceef6bda55f7b4623b5a1b
Justinshakes <Superjustin44@gmail.com>
2023-06-09 11:30:10 -0700

    Code wars
    - Char value

--- code-wars/src/KYU_8/Calculate_average.java
+++ code-wars/src/KYU_8/Calculate_average.java
@@ -1,18 +1,22 @@
 package KYU_8;
+
 import java.util.Arrays;
+
 public class Calculate_average {
-    public static double findAverage(int[] array) {
-        double total = 0;
-        for (int x : array) {
-            total += x;
-        }
-        return total / array.length;
+    public static double find_average(int[] array) {
+        // Use Arrays.stream() to create a stream of integers from the array
+        // Then, use the average() method to calculate the average of the elements in the stream
+        // If the stream is empty, return a default value of 0 using orElse(0)
+        return Arrays.stream(array).average().orElse(0);
     }
+
+    /*The orElse() method is triggered when the average() method
+    called on the stream does not have a value to return.*/
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
 
-        System.out.println("Array: " + java.util.Arrays.toString(arr));
-        System.out.println("Average: " + findAverage(arr));
+        System.out.println("Array: " + Arrays.toString(arr));
+        System.out.println("Average: " + find_average(arr));
     }
 }
