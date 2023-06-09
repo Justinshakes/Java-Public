2c1a179ec076d6d3291abde8a93da27cddb9b5ac
Justinshakes <Superjustin44@gmail.com>
2023-06-08 21:51:44 -0700

    CodeWars
    Binary to Decimal

---
+++ code-wars/src/KYU_8/Converter.java
@@ -0,0 +1,22 @@
+package KYU_8;
+
+public class Converter {
+    public static int binToDecimal(String inp) {
+        int total = 0;
+        for (int i = inp.length() - 1; i >= 0; i--) {
+            if (inp.charAt(i) == '1') {
+                int exponent = inp.length() - 1 - i;
+                total += Math.pow(2, exponent);
+            }
+        }
+        return total;
+    }
+
+    // Figure this out.
+
+    public static void main(String[] args) {
+        String s = "1001001";
+        System.out.println(s.length());
+        System.out.println("Binary: 1001001" + "\nDecimal: " + binToDecimal("1001001"));
+    }
+}
