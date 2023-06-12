d956de6b8cd2dafe2eed0a2c7e6b9e746c500b35
Justin Sherrell <Superjustin44@gmail.com>
2023-06-11 22:24:06 -0700

    Create Sumbetween.java

---
+++ code-wars/src/KYU_8/Sumbetween.java
@@ -0,0 +1,14 @@
+package KYU_8;
+
+public class Sumbetween {
+    public static int GetSum(int a, int b)
+    {
+        //Good luck!
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(GetSum(-1,2));
+
+    }
+}
