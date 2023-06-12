ae9985ef0c7325b2a2ef91edb37e2a949a2a9416
Justin Sherrell <Superjustin44@gmail.com>
2023-06-11 22:31:06 -0700

    Update Sumbetween.java

--- code-wars/src/KYU_8/Sumbetween.java
+++ code-wars/src/KYU_8/Sumbetween.java
@@ -1,14 +1,19 @@
 package KYU_8;
 
 public class Sumbetween {
-    public static int GetSum(int a, int b)
-    {
-        //Good luck!
-        return 0;
+    public static int GetSum(int a, int b) {
+        if (a == b)
+            return a;
+
+        int total = 0;
+        for (int i = (a < b) ? a : b; i <= ((a < b) ? b : a); i++) {
+            total += i;
+        }
+
+        return total;
     }
 
     public static void main(String[] args) {
-        System.out.println(GetSum(-1,2));
-
+        System.out.println(GetSum(0, -1));
     }
 }
