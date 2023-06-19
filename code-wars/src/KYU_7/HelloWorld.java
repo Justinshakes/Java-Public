e96b82cbe40d3ce1946d0725b8cf6ae29a7dd97a
Justin Sherrell <Superjustin44@gmail.com>
2023-06-19 14:20:05 -0700

    CodeWars: HelloWorld

--- code-wars/src/KYU_7/HelloWorld.java
+++ code-wars/src/KYU_7/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
         StringBuilder sb = new StringBuilder();
 
         for (int c : chars) {
-            sb.append(Character.toString((char) c));
+            sb.append(((char) c));
         }
 
         return sb.toString();
