539ca6f725439b589531641ef65ec5225b540493
Justinshakes <Superjustin44@gmail.com>
2023-06-22 23:47:08 -0700

    Update Main.java

--- Java Exercises/OOP/src/Exercise_6/Main.java
+++ Java Exercises/OOP/src/Exercise_6/Main.java
@@ -1,5 +1,7 @@
 package Exercise_6;
 
+// OOP Practice
+
 /*
     6. Write a Java program to create a class called "Employee" with a name,
     job title, and salary attributes, and methods to calculate and update salary.
